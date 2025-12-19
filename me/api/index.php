93c38b0d84d1f286ecef8108ffcdc7335df96632
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 18:04:37 +0300

    wimp

--- me/api/index.php
+++ me/api/index.php
@@ -1,19 +1,22 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Http\Kernel;
 
 define('LARAVEL_START', microtime(true));
 
+// Load Composer autoloader
 require __DIR__.'/../vendor/autoload.php';
 
+// Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Kernel::class);
 
+// Handle request
 $response = $kernel->handle(
     $request = Request::capture()
 );
 
 $response->send();
-
 $kernel->terminate($request, $response);
