5e11b7e7cacb580d10ad188a229a97910500451a
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 19:38:51 +0300

    msm

--- me/resources/views/welcome.blade.php
+++ me/resources/views/welcome.blade.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Portfolio</title>
-
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-  <div id="app"></div>
-</body>
-</html>
+<link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+<script type="module" src="{{ asset('build/assets/app.js') }}"></script>
