64b68c3d5cec5a9143b43da449627d06b604b634
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 18:35:49 +0300

    wimp

--- me/api/index.php
+++ me/api/index.php
@@ -10,6 +10,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
+$app->useStoragePath(sys_get_temp_dir());
 
 $kernel = $app->make(Kernel::class);
 
