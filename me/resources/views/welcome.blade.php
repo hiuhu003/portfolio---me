032a68ef75455bd644c22c171dfa4a336d32d60e
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 20:32:49 +0300

    sike3

--- me/resources/views/welcome.blade.php
+++ me/resources/views/welcome.blade.php
@@ -1,2 +1,1 @@
-<link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-<script type="module" src="{{ asset('build/assets/app.js') }}"></script>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
