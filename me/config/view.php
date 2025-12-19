759f093365f39e6c3837ecd6bba100ada4d6822f
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 18:56:30 +0300

    wimp

---
+++ me/config/view.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Where your Blade templates are located.
+    |
+    */
+
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This determines where Blade stores compiled templates. On Vercel, we
+    | must use /tmp because storage/ is read-only.
+    |
+    */
+
+    'compiled' => env('VIEW_COMPILED_PATH', '/tmp'),
+
+];
