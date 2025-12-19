0e3383af8e78992f1ce567d418377cebb21c4df0
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 20:49:16 +0300

    sike 3.3

--- me/resources/views/welcome.blade.php
+++ me/resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
+@php
+    $manifestPath = public_path('build/manifest.json');
+    echo "Manifest exists: " . (file_exists($manifestPath) ? 'YES' : 'NO') . "<br>";
+    echo "Looking at: " . $manifestPath . "<br>";
+    if (file_exists(public_path('build'))) {
+        echo "Build directory contents: <br>";
+        print_r(scandir(public_path('build')));
+    }
+@endphp
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
