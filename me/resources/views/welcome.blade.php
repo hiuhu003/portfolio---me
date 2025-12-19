418d90dd53ff78ccd1f5c10a712d2f545e2fedb1
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 21:26:34 +0300

    hopes minus

--- me/resources/views/welcome.blade.php
+++ me/resources/views/welcome.blade.php
@@ -1,1 +1,16 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Portfolio</title>
+    @if(file_exists(public_path('build/manifest.json')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link rel="stylesheet" href="/build/assets/app-BLthNWWj.css">
+        <link rel="stylesheet" href="/build/assets/app-DJTf-ORP.css">
+        <script type="module" src="/build/assets/app-Cz9ctsU2.js"></script>
+    @endif
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
