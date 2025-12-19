c533ffd93486cc88f14cf5e3a863440fa25c56d9
Ursula <ursulahiuhu033@gmail.com>
2025-12-19 18:59:23 +0300

    wimp

--- me/api/index.php
+++ me/api/index.php
@@ -13,7 +13,6 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->useStoragePath(sys_get_temp_dir());
 
-config(['view.compiled' => sys_get_temp_dir()]);
 $kernel = $app->make(Kernel::class);
 
 // Handle request
